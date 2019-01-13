2969a6d703854354d873900887387a93502b17c3
TheCapHunter <46630258+TheCapHunter@users.noreply.github.com>
2019-01-13 19:01:41 +0000

    Create Mateo.py

---
+++ Mateo.py
@@ -0,0 +1,90 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import platform
+import colorsys
+import random
+import os
+import time
+from discord.voice_client import VoiceClient
+from discord import Game, Embed, Color, Status, ChannelType
+
+
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
+client = Bot(description="Mateo is best", command_prefix="d!", pm_help = True)
+client.remove_command('help')
+
+
+async def status_task():
+    while True:
+        await client.change_presence(game=discord.Game(name='d!help'))
+        
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to 'str(len(client.servers))+' servers | Connected to 'str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Started Dark BOT')
+    print('Created by Utkarsh')
+    client.loop.create_task(status_task())
+
+
+  
+
+def is_owner(ctx):
+    return ctx.message.author.id == "420525168381657090, 395535610548322326"
+
+def is_dark(ctx):
+    return ctx.message.author.id == "420525168381657090"
+
+def is_shreyas(ctx):
+    return ctx.message.author.id == "376602841625919488"
+
+def is_gameworld(ctx):
+    return ctx.message.author.id == "402075464694366211"
+
+def is_ranger(ctx):
+    return ctx.message.author.id == "304911836460089345"
+
+@client.command(pass_context = True)
+@commands.check(is_owner)
+async def restart():
+    await client.logout()
+
+@client.event
+async def on_message(message):
+	await client.process_commands(message)
+
+@client.event
+async def on_member_join(member):
+    print("In our server" + member.name + " just joined")
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Welcome message')
+    embed.add_field(name = '__Welcome to Our Server__',value ='**Hope you will be active here. Check Our server rules and never try to break any rules. ',inline = False)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    await client.send_message(member,embed=embed)
+    print("Sent message to " + member.name)
+    channel = discord.utils.get(client.get_all_channels(), server__name='DarkBot Official Server', name='darkbot-servers-join-leave-log')
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check <#474572305192845312> and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+    embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
+    embed.add_field(name='Your join position is', value=member.joined_at)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    embed.set_thumbnail(url=member.avatar_url)
+    await client.send_message(channel, embed=embed)
+
+
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True)
+
+async def unbanall(ctx):
+    server=ctx.message.server
+    ban_list=await client.get_bans(server)
+    await client.say('Unbanning {} members'.format(len(ban_list)))
+    for member in ban_list:
+        await client.unban(server,member)
+
+client.run('NTMyMjg5NTUzNTU4NDA1MTQx.DxagPg.J3SHfnkoCVxCKbfaX0Zf-cVw-GA')
